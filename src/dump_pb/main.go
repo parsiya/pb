c51efb0518d2e816c4e2254550089a8cc1856c3c
Doug Fort <doug.fort@gmail.com>
2013-09-22 18:15:19 -0400

    basic parsing in parse_pb

--- src/dump_pb/main.go
+++ src/dump_pb/main.go
@@ -7,60 +7,29 @@
 package main
 
 import (
-   "bufio"
-   "fmt"
+   "parse_pb"
    "io"
    "log"
    "os"
 )
 
-/*
-the "Banana" protocol works by serializing lists.
-look at banana.py line 150 (dataReceived).  define same constants.
-*/
-const (
-	PB_LIST      byte = 128
-	PB_INT       byte = 129
-	PB_STRING    byte = 130
-	PB_NEG       byte = 131
-	PB_FLOAT     byte = 132
-	PB_LONGINT   byte = 133
-	PB_LONGNEG   byte = 134
-	PB_VOCAB     byte = 135
-	HIGH_BIT_SET byte = 128
-)
-
-var (
-	pb_constants = map[byte]string{
-		128 : "PB_LIST",
-		129 : "PB_INT",
-		130 : "PB_STRING",
-		131 : "PB_NEG",
-		132 : "PB_FLOAT",
-		133 : "PB_LONGINT",
-		134 : "PB_LONGNEG",
-		135 : "PB_VOCAB"}
-)
 
 // main entry point for ergo_prpoxy
 func main() {
 	log.Printf("INFO: program starts")
-	reader := bufio.NewReader(os.Stdin)
-	var c byte
-	var err error
+	parser, err := parse_pb.NewParser(os.Stdin)
+	if err != nil {
+		log.Fatalf("CRITICAL: parse_pb.NewParser failed %s", err)
+	}
 	for {
-		if c, err = reader.ReadByte(); err != nil {
+		result, err := parser.Step()
+		if err != nil {
 			if err == io.EOF {
 				break
 			}
 			log.Fatalf("CRITICAL: error on ReadByte %s", err)
 		}
-		constant, ok := pb_constants[c]
-		if ok {
-			fmt.Printf("%s\n", constant)
-		} else {
-			fmt.Printf("%d\n", c)
-		}
+		log.Printf("DEBUG: %s", result)
 	}
 	log.Printf("INFO: program ends")
 }
---
+++ src/parse_pb/parse_pb.go
@@ -0,0 +1,203 @@
+/* package parse_pb
+
+This package interprets a stream of PB bytes
+*/
+package parse_pb
+
+/*
+the "Banana" protocol works by serializing lists.
+look at banana.py line 150 (dataReceived).  define same constants.
+*/
+import (
+	"bufio"
+   	"fmt"
+   	"io"
+   	"strings"
+)
+
+const (
+	PB_LIST      byte = 128
+	PB_INT       byte = 129
+	PB_STRING    byte = 130
+	PB_NEG       byte = 131
+	PB_FLOAT     byte = 132
+	PB_LONGINT   byte = 133
+	PB_LONGNEG   byte = 134
+	PB_VOCAB     byte = 135
+	HIGH_BIT_SET byte = 128
+)
+
+var (
+	pb_constants = map[byte]string{
+		128 : "PB_LIST",
+		129 : "PB_INT",
+		130 : "PB_STRING",
+		131 : "PB_NEG",
+		132 : "PB_FLOAT",
+		133 : "PB_LONGINT",
+		134 : "PB_LONGNEG",
+		135 : "PB_VOCAB"}
+)
+
+type parseItem interface {
+	Type() byte
+	String() string
+}
+
+type PBList struct {
+	value []parseItem
+}
+
+func (item PBList) Type() byte {
+	return PB_LIST
+}
+
+func (item PBList) String() string {
+	var printValues []string 
+	for _, x := range item.value {
+		printValues = append(printValues, x.String()) 
+	}
+	return fmt.Sprintf("PB_LIST(%s)", strings.Join(printValues, ","))
+} 
+
+type PBInt struct {
+	value int
+}
+
+func (item PBInt) String() string {
+	return fmt.Sprintf("PB_INT(%d)", item.value)
+} 
+
+func (item PBInt) Type() byte {
+	return PB_INT
+}
+
+type PBString struct {
+	value string
+}
+
+func (item PBString) String() string {
+	return fmt.Sprintf("PB_STRING(%s)", item.value)
+} 
+
+func (item PBString) Type() byte {
+	return PB_STRING
+}
+
+type PBVocab struct {
+	value int
+}
+
+func (item PBVocab) Type() byte {
+	return PB_VOCAB
+}
+
+func (item PBVocab) String() string {
+	return fmt.Sprintf("PB_VOCAB(%d)", item.value)
+} 
+
+type PBUnknown struct {
+	i byte
+	c byte
+	data []byte
+}
+
+func (item PBUnknown) String() string {
+	return fmt.Sprintf("* %d %s %X *", item.i, dumpByte(item.c), item.data)
+}
+
+func (item PBUnknown) Type() byte {
+	return 0
+}
+
+type Parser struct {
+	reader *bufio.Reader
+}
+
+func NewParser(reader io.Reader) (*Parser, error) {
+	parser := Parser{reader : bufio.NewReader(reader)}
+	return &parser, nil
+}
+
+func (parser *Parser) Step() (parseItem, error) {
+	var i byte
+	var c byte
+	var err error
+	if i, err = parser.reader.ReadByte(); err != nil {
+		return nil, err
+	}
+	if c, err = parser.reader.ReadByte(); err != nil {
+		return nil, err
+	}
+	switch c {
+	case PB_LIST:
+		return parser.parseList(i, c)
+	case PB_INT:
+		return parser.parseInt(i, c)
+	case PB_STRING:
+		return parser.parseString(i, c)
+	case PB_VOCAB:
+		return parser.parseVocab(i, c)
+	}
+	return parser.parseUnknown(i, c)
+}
+
+func (parser *Parser) parseList(i byte, _ byte) (parseItem, error) {
+	size := int(i)
+	list := PBList{value : make([]parseItem, int(size))}
+	for j := 0; j < size; j++ {
+		value, err := parser.Step()
+		if err != nil {
+			return nil, err
+		}
+		list.value[j] = value 
+	}
+
+	return list, nil
+}
+
+func (parser *Parser) parseInt(i byte, _ byte) (parseItem, error) {
+	return PBInt{value : int(i)}, nil
+}
+
+func (parser *Parser) parseString(i byte, _ byte) (parseItem, error) {
+	size := int(i)
+	buffer := make([]byte, size)
+	n, err := parser.reader.Read(buffer)
+	if err != nil {
+		return nil, err
+	}
+	if n != size {
+		return nil, 
+			fmt.Errorf("parserString read %d bytes expecting %d", n, size)
+	}
+	return PBString{value : string(buffer)}, nil
+}
+
+func (parser *Parser) parseVocab(i byte, _ byte) (parseItem, error) {
+	return PBVocab{value : int(i)}, nil
+}
+
+func (parser *Parser) parseUnknown(i byte, c byte) (parseItem, error) {
+	size := int(i)
+	buffer := make([]byte, size)
+	n, err := parser.reader.Read(buffer)
+	if err != nil {
+		return nil, err
+	}
+	if n != size {
+		return nil, 
+			fmt.Errorf("parseUnknown read %d bytes expecting %d", n, size)
+	}
+	return PBUnknown{i : i, c : c, data : buffer}, nil
+}
+
+
+// dumpByte dumps one byte
+func dumpByte(c byte) string {
+	constant, ok := pb_constants[c]
+	if ok {
+		return constant
+	}
+	return fmt.Sprintf("%d", c)
+}
